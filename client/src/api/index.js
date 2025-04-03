8af00e29ae67580066637692b72c53ac4881058b
alialishoaibali1254 <alialishoaibali719296@gmail.com>
2025-04-03 01:20:33 -0700

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:8080/api/",
+    baseURL: "fitness-track-back.vercel.app",
   });
 
   export const UserSignUp = async (data) => API.post("/user/signup", data);
