58a4faba578c0345e077936399503f2bca7d1769
alialishoaibali1254 <alialishoaibali719296@gmail.com>
2025-04-04 03:31:03 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,26 +2,17 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 import mongoose from "mongoose";
-import UserRoutes from "./routes/User.js"
+import UserRoutes from "./routes/User.js";
 
 dotenv.config();
+
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true })); // for form data
 
-
-
-
-app.get("/", async (req, res) => {
-    res.status(200).json({
-      message: "Hello developers from GFG",
-    });
-  });
-
-  app.use("/api/user", UserRoutes);
-
- // error handler
+app.use("/api/user/", UserRoutes);
+// error handler
 app.use((err, req, res, next) => {
   const status = err.status || 500;
   const message = err.message || "Something went wrong";
@@ -31,20 +22,31 @@ app.use((err, req, res, next) => {
     message,
   });
 });
-  const connectDB = () => {
-   mongoose.set("strictQuery", true);
-   mongoose.connect(process.env.MONGODB_URL)
-   .then((res) => console.log("connected to mongodb"))
-   .catch((err)=>{
-    console.log(err);
-   })
-  };
+
+app.get("/", async (req, res) => {
+  res.status(200).json({
+    message: "Hello developers from GFG",
+  });
+});
+
+const connectDB = () => {
+  mongoose.set("strictQuery", true);
+  mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() => console.log("Connected to Mongo DB"))
+    .catch((err) => {
+      console.error("failed to connect with mongo");
+      console.error(err);
+    });
+};
+
 const startServer = async () => {
-    try {
-        connectDB();
-      app.listen(8080, () => console.log("Server started on port 8080"));
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  startServer();
+  try {
+    connectDB();
+    app.listen(8080, () => console.log("Server started on port 8080"));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
